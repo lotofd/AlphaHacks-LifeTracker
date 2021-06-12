1c6d169a6e9a353d011e7ff7dab37afd065d9e39
ArjunCodes29 <lichessreck123@gmail.com>
2021-06-12 17:11:15 +0530

    Made functioning scheduler
    added css to make it look presentable
    added most of js

---
+++ Scheduler/script.js
@@ -0,0 +1,105 @@
+const breakTask = document.getElementById('break');
+const gymTask = document.getElementById('gym');
+const studyTask = document.getElementById('study');
+const tvTask = document.getElementById('tv');
+const friendsTask = document.getElementById('friends');
+const workTask = document.getElementById('work');
+const taskContainer = document.querySelector('.task__container');
+const scheduleContainer = document.querySelector('.schedule__container');
+const resetBtn = document.querySelector('.deleteBtn');
+const popUp = document.querySelector('.pop-up__container');
+const noBtn = document.getElementById('btn__no');
+const yesBtn = document.getElementById('btn__yes');
+
+
+let selectedColor, active;
+
+//Event Listeners
+taskContainer.addEventListener('click', selectTask);
+scheduleContainer.addEventListener('click', setColors);
+resetBtn.addEventListener('click',openPopup);
+noBtn.addEventListener('click', closePopup);
+yesBtn.addEventListener('click', deleteTasks);
+
+// Tasks click  (3)
+function selectTask (e){
+    resetTasks()
+
+    taskColor = e.target.style.backgroundColor;
+
+    switch(e.target.id){
+        case 'work':
+            activeTask(workTask, );
+            icon= String("Work");
+            break
+        case 'break':
+            activeTask(breakTask, );
+            icon= String("Break");
+            break
+        case 'gym':
+            activeTask(gymTask, );
+            icon= String("Gym");
+            break
+        case 'study':
+            activeTask(studyTask, );
+            icon= String(" Study");
+            break
+        case 'tv':
+            activeTask(tvTask, );
+            icon= String("TV");
+            break
+        case 'friends':
+            activeTask(friendsTask, );
+            icon= String("Friend");
+            break
+
+        
+    }
+
+};
+
+// Set colors for schedule (4)
+function setColors (e){
+    if(e.target.classList.contains('task') && active === true){
+        e.target.style.backgroundColor = selectedColor;
+        e.target.innerHTML = icon;
+    }else if(e.target.classList.contains('fas') && active === true){
+        e.target.parentElement.style.backgroundColor = selectedColor;
+        e.target.parentElement.innerHTML = icon;
+    }
+};
+
+// Active task (1)
+function activeTask(task, color){
+    task.classList.toggle('selected');
+
+    if(task.classList.contains('selected')){
+        active = true;
+        selectedColor = color;
+        return selectedColor;
+    } else {
+        active = false;
+    }
+}
+
+// Reset tasks (2)
+function resetTasks(){
+    const allTasks = document.querySelectorAll('.task__name');
+
+    allTasks.forEach((item)=>{
+        item.className = 'task__name';
+    })
+}
+
+// Delete tasks
+function deleteTasks(){
+    const tasks = document.querySelectorAll('.task');
+
+    tasks.forEach((item)=>{
+        item.innerHTML = '';
+        item.style.backgroundColor = "#DDB4FF";
+    })
+
+    closePopup();
+}
+
