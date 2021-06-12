81f22a332724e478d1f9357a56dc8e3ad43a8956
ArjunCodes29 <lichessreck123@gmail.com>
2021-06-12 19:57:13 +0530

    Finished scheduler
    Added Css
    added more buttons
    added reset functionality
    added card

--- Scheduler/script.js
+++ Scheduler/script.js
@@ -6,7 +6,7 @@ const friendsTask = document.getElementById('friends');
 const workTask = document.getElementById('work');
 const taskContainer = document.querySelector('.task__container');
 const scheduleContainer = document.querySelector('.schedule__container');
-const resetBtn = document.querySelector('.deleteBtn');
+const resetBtn = document.querySelector('.reset-btn');
 const popUp = document.querySelector('.pop-up__container');
 const noBtn = document.getElementById('btn__no');
 const yesBtn = document.getElementById('btn__yes');
@@ -17,63 +17,78 @@ let selectedColor, active;
 //Event Listeners
 taskContainer.addEventListener('click', selectTask);
 scheduleContainer.addEventListener('click', setColors);
-resetBtn.addEventListener('click',openPopup);
+resetBtn.addEventListener('click', openPopup);
 noBtn.addEventListener('click', closePopup);
 yesBtn.addEventListener('click', deleteTasks);
 
 // Tasks click  (3)
-function selectTask (e){
+function selectTask(e) {
     resetTasks()
 
     taskColor = e.target.style.backgroundColor;
 
-    switch(e.target.id){
+    switch (e.target.id) {
         case 'work':
             activeTask(workTask, );
-            icon= String("Work");
+            icon = String("Work");
             break
         case 'break':
             activeTask(breakTask, );
-            icon= String("Break");
+            icon = String("Break");
             break
         case 'gym':
             activeTask(gymTask, );
-            icon= String("Gym");
+            icon = String("Gym");
             break
         case 'study':
             activeTask(studyTask, );
-            icon= String(" Study");
+            icon = String("Maths");
             break
         case 'tv':
             activeTask(tvTask, );
-            icon= String("TV");
+            icon = String("TV");
             break
         case 'friends':
             activeTask(friendsTask, );
-            icon= String("Friend");
+            icon = String("Friend");
+            break
+        case 'eng':
+            activeTask(workTask, );
+            icon = String(" English");
+            break
+        case 'chores':
+            activeTask(workTask, );
+            icon = String("Chores");
+            break
+        case 'family':
+            activeTask(workTask, );
+            icon = String("Family");
+            break
+        case 'sci':
+            activeTask(workTask, );
+            icon = String(" Sci");
             break
 
-        
     }
 
 };
 
 // Set colors for schedule (4)
-function setColors (e){
-    if(e.target.classList.contains('task') && active === true){
+function setColors(e) {
+    if (e.target.classList.contains('task') && active === true) {
         e.target.style.backgroundColor = selectedColor;
         e.target.innerHTML = icon;
-    }else if(e.target.classList.contains('fas') && active === true){
+    } else if (e.target.classList.contains('fas') && active === true) {
         e.target.parentElement.style.backgroundColor = selectedColor;
         e.target.parentElement.innerHTML = icon;
     }
 };
 
 // Active task (1)
-function activeTask(task, color){
+function activeTask(task, color) {
     task.classList.toggle('selected');
 
-    if(task.classList.contains('selected')){
+    if (task.classList.contains('selected')) {
         active = true;
         selectedColor = color;
         return selectedColor;
@@ -83,23 +98,31 @@ function activeTask(task, color){
 }
 
 // Reset tasks (2)
-function resetTasks(){
+function resetTasks() {
     const allTasks = document.querySelectorAll('.task__name');
 
-    allTasks.forEach((item)=>{
+    allTasks.forEach((item) => {
         item.className = 'task__name';
     })
 }
 
 // Delete tasks
-function deleteTasks(){
+function deleteTasks() {
     const tasks = document.querySelectorAll('.task');
 
-    tasks.forEach((item)=>{
+    tasks.forEach((item) => {
         item.innerHTML = '';
-        item.style.backgroundColor = "#DDB4FF";
+        item.style.backgroundColor = "#D3D3D3";
     })
 
     closePopup();
 }
+// Open Pop-up
+function openPopup(){
+    popUp.style.display = 'flex';
+}
 
+// Close Pop-up
+function closePopup(){
+    popUp.style.display = 'none';
+}
