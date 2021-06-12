15ba952c122b8bb56477abcf52d1e8e4a01268ff
ArjunCodes29 <lichessreck123@gmail.com>
2021-06-12 10:42:34 +0530

    Js and Html
    Removed placeholder code
    
    added the js
    
    (referenced from yotuube videohttps://www.youtube.com/watch?v=unrmB4H7Wmw

---
+++ Journal/script.js
@@ -0,0 +1,103 @@
+// Add note to local storage
+let addBtn = document.getElementById("svejournal");
+addBtn.addEventListener("click", function(e) {
+
+  let addTitle = document.getElementById("note-title");
+  let addTxt = document.getElementById("note-text");
+  
+    if (addTitle.value == "" || addTxt.value == "") {
+        return alert("Make sure to fill out both the date and the journal!")
+    }
+
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+  let myObj = {
+    title: addTitle.value,
+    text: addTxt.value
+  }
+  notesObj.push(myObj);
+  localStorage.setItem("notes", JSON.stringify(notesObj));
+  addTxt.value = "";
+  addTitle.value = "";
+//   console.log(notesObj);
+  showNotes();
+});
+
+// Function to show elements from localStorage
+function showNotes() {
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+  let html = "";
+  notesObj.forEach(function(element, index) {
+    html += `
+        <div class="note">
+            <h3 class="note-title"> ${element.title} </h3>
+            <p class="note-text"> ${element.text}</p>
+            <button id="${index}"onclick="deleteNote(this.id)" class="note-btn">Delete Entry</button>
+            <button id="${index}"onclick="editNote(this.id)" class="note-btn edit-btn">Edit Entry</button>
+        </div>
+            `;
+  });
+  let notesElm = document.getElementById("notes");
+  if (notesObj.length != 0) {
+    notesElm.innerHTML = html;
+  } else {
+    notesElm.innerHTML = `<br> <p style="color:white" id="message">Start journalling today and experience its many benefits!</p>`;
+  }
+}
+
+// Function to delete a note
+function deleteNote(index) {
+//   console.log("I am deleting", index);
+    let confirmDel = confirm("Are You sure You want to delete this");
+    if (confirmDel == true) {
+        let notes = localStorage.getItem("notes");
+        if (notes == null) {
+            notesObj = [];
+        } else {
+            notesObj = JSON.parse(notes);
+        }
+
+        notesObj.splice(index, 1);
+        localStorage.setItem("notes", JSON.stringify(notesObj));
+        showNotes();
+    }
+  
+}
+
+// Function to Edit the Note
+function editNote(index) {
+    let notes = localStorage.getItem("notes");
+    let addTitle = document.getElementById("note-title");
+    let addTxt = document.getElementById("note-text");
+
+    if (addTitle.value !== "" || addTxt.value !== "") {
+      return alert("Please clear the form before editing a note")
+    } 
+
+    if (notes == null) {
+      notesObj = [];
+    } else {
+      notesObj = JSON.parse(notes);
+    }
+    console.log(notesObj);
+
+    notesObj.findIndex((element, index) => {
+      addTitle.value = element.title;
+      addTxt.value = element.text;
+    })
+    notesObj.splice(index, 1);
+        localStorage.setItem("notes", JSON.stringify(notesObj));
+        showNotes();
+}
+
+
+showNotes();
